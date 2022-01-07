d2f1cd5dee9bd2ce56defba36e3bd79269f3547d
bricio123 <fabricio1234c@gmail.com>
2022-01-06 23:11:28 -0300

    feat function create user

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid')
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const customers = [];
 //para qualquer criação de um dado usamos o metodo POST
 app.post("/account", (request, response) => {
-    const {cpf, name} = request.body;
-    const id = uuidv4();
+  const { cpf, name } = request.body;
+  
 
-    customers.push({
-        cpf,
-        name,
-        id,
-        statement:[],
-    })
-    return  response.status(201).send()
-})
-app.listen(3333)
+  const customersAlreadyExists = customers.some(
+    (customers) => customers.cpf === cpf
+  );
+
+  if (customersAlreadyExists) {
+      return response.status(400).json({error:"Customers Already Exists!"})
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+  return response.status(201).send();
+});
+app.listen(3333);
